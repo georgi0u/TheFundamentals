3b506fcabfa72d5b203e7518cb54e49bbdf1753b
Adam Georgiou <me@adamgeorgiou.com>
2014-07-26 20:16:46 -0400

    Got the maze generating working, using a version of Prim's algorithm.

--- problems/maze/maze.py
+++ problems/maze/maze.py
@@ -3,29 +3,46 @@
 import random
 
 class Graph(object):
+    """
+    A graph class implemented using adjacency lists.
+    """
     def __init__(self):
         self._adjacency_list = dict()
 
     def adjacent(self, node_1, node_2):
+        """
+        Returns true if node_1 and node_2 are adjacent to one another, else
+        otherwise. Expects node_1 and node_2 to be in the graph.
+        """
         if(node_1 not in self._adjacency_list or
            node_2 not in self._adjacency_list):
             raise Exception('Node doesn\'t exist')
 
         return node_1 in self._adjacency_list[node_2]
 
-    def neighbors(self, node_1):
-        if node_1 not in self._adjacency_list:
+    def neighbors(self, node):
+        """
+        Returns a list of the nodes adjacent to `node`. Expects `node` to be
+        in the graph.
+        """
+        if node not in self._adjacency_list:
             raise Exception('Node doesn\'t exist')
 
-        return  self._adjacency_list[node_1]
+        return  self._adjacency_list[node]
 
     def add_node(self, node):
+        """
+        Adds `node` to the graph.
+        """
         if node in self._adjacency_list:
             raise Exception('Node already exists')
 
         self._adjacency_list[node] = []
 
     def add_edge(self, node_1, node_2):
+        """
+        Adds an edge between `node_1` and `node_2`.
+        """
         def add_directed_edge(node_1, node_2):
             if node_1 not in self._adjacency_list:
                 raise Exception('Node doesn\'t exist')
@@ -35,6 +52,9 @@ class Graph(object):
         add_directed_edge(node_2, node_1)
 
     def remove_edge(self, node_1, node_2):
+        """
+        Removes all edges between `node_1` and `node_2`.
+        """
         if(node_1 not in self._adjacency_list or
            node_2 not in self._adjacency_list):
             raise Exception('Node doesn\'t exist') 
@@ -42,15 +62,17 @@ class Graph(object):
         self._adjacency_list[node_1].remove(node_2)
         self._adjacency_list[node_2].remove(node_1)
 
-class Maze(Graph):
+class Maze(object):
     def __init__(self, width):
-        Graph.__init__(self)
+        self._graph = Graph()
 
         self._width = width
+        self._cells_in_maze = []
 
         for row in range(0, width):
             for col in range(0, width):
-                self.add_node((row, col))
+                node = (row, col)
+                self._graph.add_node(node)
 
         self.prim_init()
 
@@ -59,172 +81,70 @@ class Maze(Graph):
 
     def surrounding_cells(self, x, y):
         def get_deltas(coordinate):
-            delta_coordinates = []
-            deltas = [-1, 1]
-            for delta in deltas:
-                neighbor = coordinate + delta
-                if self._is_in_bound(neighbor):
-                    delta_coordinates.append(neighbor)
-            return delta_coordinates
+            return filter(self._is_in_bound, [coordinate + d for d in [-1, 1]])
 
         ret = []
         ret.extend([(dx, y) for dx in get_deltas(x)])
         ret.extend([(x, dy) for dy in get_deltas(y)])
         return ret
 
+    def test_init(self):
+        for i in range(0, self._width-1):
+            node_1 = (i,0)
+            node_2 = (i+1,0)
+            self._graph.add_edge((i, 0), (i+1, 0))
+
     def prim_init(self):
+        start_node = (
+            random.randint(0,self._width-1), 
+            random.randint(0,self._width-1)
+        )
+        self._cells_in_maze.append(start_node)
+        frontier = self.surrounding_cells(*start_node)
 
-        frontier = [(0,0)]
-        while len(frontier) != 0:
-            frontier_cell = random.choice(frontier)
-            frontier.remove(frontier_cell)
+        while frontier: 
+            frontier_choice = random.choice(frontier)
+            frontier.remove(frontier_choice)
 
-            neighbor_cell = random.choice(
-                self.surrounding_cells(*frontier_cell)
+            surrounding_cells = filter(
+                lambda x: x in self._cells_in_maze,
+                self.surrounding_cells(*frontier_choice)
             )
 
-            self.add_edge(frontier_cell, neighbor_cell)
+            neighbor = random.choice(surrounding_cells)
 
-            for surrounding_cell in self.surrounding_cells(*neighbor_cell):
-                if len(self.neighbors(surrounding_cell)) == 0:
-                    frontier.append(surrounding_cell)
+            self._graph.add_edge(frontier_choice, neighbor)
+            self._cells_in_maze.append(frontier_choice)
 
-            print self.__str__()
+            new_frontier = filter(
+                lambda x: x not in self._cells_in_maze,
+                self.surrounding_cells(*frontier_choice)
+            )
 
+            new_frontier = filter(
+                lambda x: x not in frontier,
+                new_frontier)
 
+            frontier.extend(new_frontier)
 
     def __unicode__(self):
         string = u' '
         string += '_'*(self._width*2-1)
         string += '\n'
-        for row in range(0, self._width):
+        for col in range(0, self._width):
             string += '|'
-            for col in range(0, self._width):
-                neighbors = self._adjacency_list[(row, col)]
-                string += '_' if (col, row+1) not in neighbors else ' '
-                string += '|' if (col+1, row) not in neighbors else ' '
-
+            for row in range(0, self._width):
+                neighbors = self._graph.neighbors((row, col))
+                string += '_' if (row, col+1) not in neighbors else ' '
+                string += '|' if (row+1, col) not in neighbors else ' '
             string += '\n'
         string += '\n'
+
         return string
 
     def __str__(self):
         return unicode(self).encode('utf-8')
 
+maze = Maze(50)
 
-    # def __init__(self, width):
-    #     self._adjacency_matrix = []
-    #     for i in range(0, width**2):
-    #         self._adjacency_matrix.append([0] * width**2)
-
-    #     current_point = (0, 0)
-    #     self._adjacency_matrix[0][0] = 1
-
-    #     wall_list = []
-    #     for neighbor in self.neighbors(*current_point):
-    #         if not self.is_free(*neighbor):
-    #             wall_list.append(neighbor)
-
-    #     while len(wall_list) != 0:
-
-    #         wall = random.choice(wall_list)
-    #         wall_list.remove(wall)
-
-    #         new_current = None
-    #         if wall[0] > current_point[0] and self._is_in_bounds(wall[0] + 1):
-    #             new_current = (wall[0] + 1, wall[1])
-    #         elif wall[0] < current_point[0] and self._is_in_bounds(wall[0] - 1):
-    #             new_current = (wall[0] - 1, wall[1])
-    #         elif wall[1] > current_point[1] and self._is_in_bounds(wall[1] + 1):
-    #             new_current = (wall[0], wall[1] + 1)
-    #         elif wall[1] < current_point[1] and self._is_in_bounds(wall[1] - 1):
-    #             new_current = (wall[0], wall[1] - 1)
-
-
-
-    #         if not new_current:
-    #             continue
-
-    #         if self.is_free(*new_current):
-    #             continue
-
-    #         self.open(*wall)
-    #         self.open(*new_current)
-
-    #         neighbor_filter = lambda coordinate: self.is_wall(*coordinate)
-    #         wall_list.extend(
-    #             self.neighbors(
-    #                 *new_current, neighbor_filter = neighbor_filter
-    #             )
-    #         )
-
-    #         print self.__str__()
-    #         raw_input()
-
-
-
-
-    # def neighbors(self, x, y, neighbor_filter=None):
-    #     def get_deltas(coordinate):
-    #         delta_coordinates = []
-    #         deltas = [-1, 1]
-    #         for delta in deltas:
-    #             neighbor = coordinate + delta
-    #             try:
-    #                 self._check_bound(neighbor)
-    #                 delta_coordinates.append(neighbor)
-    #             except IndexError as error:
-    #                 pass
-    #         return delta_coordinates
-
-    #     ret = []
-    #     ret.extend([(dx, y) for dx in get_deltas(x)])
-    #     ret.extend([(x, dy) for dy in get_deltas(y)])
-    #     if neighbor_filter:
-    #         ret = filter(neighbor_filter, ret)
-
-    #     return ret
-
-    # def _is_in_bounds(self, x_or_y):
-    #     return x_or_y >= 0 and x_or_y < len(self._adjacency_matrix)
-
-    # def _check_bound(self, x_or_y):
-    #     if not self._is_in_bounds(x_or_y):
-    #         raise IndexError('Coordinate out of bounds')
-
-    # def _check_bounds(self, x, y):
-    #     [self._check_bound(coordinate) for coordinate in [x, y]]
-
-    # def is_free(self, x, y):
-    #     self._check_bounds(x, y)
-    #     return self._adjacency_matrix[x][y] == 1
-
-    # def is_wall(self, x, y):
-    #     self._check_bounds(x, y)
-    #     return self._adjacency_matrix[x][y] == 0
-
-    # def block(self, x, y):
-    #     self._check_bounds(x, y)
-    #     self._adjacency_matrix[x][y] = 0
-
-    # def open(self, x, y):
-    #     self._check_bounds(x, y)
-    #     self._adjacency_matrix[x][y] = 1
-
-    # def __unicode__(self):
-    #     string = u''
-    #     for row in self._adjacency_matrix:
-    #         for cell in row:
-    #             if cell == 1:
-    #                 string += ' '
-    #             else:
-    #                 string += u'\u2588'
-    #         string += '\n'
-    #     return string.rstrip()
-
-    # def __str__(self):
-    #     return unicode(self).encode('utf-8')
-
-
-maze = Maze(10)
 print maze
